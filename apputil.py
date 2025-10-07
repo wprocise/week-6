3501949246e77c875dd4624904fcca0c51b88731
Woods Procise <wprocise@iu.edu>
2025-10-07 13:05:32 -0400

    Access token saved and printed

--- apputil.py
+++ apputil.py
@@ -1,16 +1,21 @@
 ## Exercise 1
 """
     Download the .env file 
-    and added to the .gitignore
+    and added to the .gitignore;
+    Then create python class 'Genius' and connect
+    to the access token in the .env file
 """
 
 from dotenv import load_dotenv
 import os
 load_dotenv('week-6.env')
-access_token = os.getenv('GENIUS_ACCESS_TOKEN')
-print(access_token)
 
 class Genius:
-    def __init__(self, access_token):
-        self.access_token = access_token
-        
+    def __init__(self):
+        self.access_token = os.getenv('ACCESS_TOKEN')
+"""
+    Should print the access token
+"""
+genius = Genius()
+print(f"Access token set to: {genius.access_token}")
+
