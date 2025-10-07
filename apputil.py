df585a6a5e80f7b0da2274f342204117d404ae7f
Woods Procise <wprocise@iu.edu>
2025-10-07 12:40:33 -0400

    Downloaded .env and defining access_token

--- apputil.py
+++ apputil.py
@@ -1,1 +1,11 @@
-# your code here ...
+## Exercise 1
+"""
+    Download the .env file 
+    and added to the .gitignore
+"""
+
+from dotenv import load_dotenv
+import os
+load_dotenv('week-6.env')
+access_token = os.getenv('GENIUS_ACCESS_TOKEN')
+print(access_token)
