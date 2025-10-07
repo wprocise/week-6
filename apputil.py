3d15c614faef0df1f2adb781f6a7abcf0ef15eb9
Woods Procise <wprocise@iu.edu>
2025-10-07 12:41:24 -0400

    Creating Genius class

--- apputil.py
+++ apputil.py
@@ -9,3 +9,8 @@ import os
 load_dotenv('week-6.env')
 access_token = os.getenv('GENIUS_ACCESS_TOKEN')
 print(access_token)
+
+class Genius:
+    def __init__(self, access_token):
+        self.access_token = access_token
+        
