49b3c386e559d90321833990ad73e90ab5c240c8
Woods Procise <wprocise@iu.edu>
2025-11-12 12:39:45 -0500

    Final updates

--- apputil.py
+++ apputil.py
@@ -4,40 +4,46 @@ import requests
 import pandas as pd
 
 # Load environment variables
-load_dotenv()
+load_dotenv('week-6.env')
 
 class Genius:
-    def __init__(self, access_token=None):
-        self.access_token = os.getenv("ACCESS_TOKEN")
+    def __init__(self):
+        self.access_token = os.getenv('ACCESS_TOKEN')
     """
-    Should print the access token
+        New method to get artist information for a list of search terms
+        Getting artist information for various search terms
     """
-genius = Genius()
-print(f"ACCESS_TOKEN: {genius.access_token}")   
-
-def get_artist(self, search_term):
-        """Returns full artist JSON object for a given search term."""
-        search_url = f"https://api.genius.com/search?q={search_term}&access_token={self.access_token}"
+    def get_artist(self, search_term):
+        search_url = f"http://api.genius.com/search?q={search_term}&access_token={self.access_token}"
         response = requests.get(search_url)
         json_data = response.json()
-
         artist_id = json_data['response']['hits'][0]['result']['primary_artist']['id']
-        artist_url = f"https://api.genius.com/artists/{artist_id}?access_token={self.access_token}"
+        artist_url = f"http://api.genius.com/artists/{artist_id}?access_token={self.access_token}"
         artist_response = requests.get(artist_url)
-        return artist_response.json()
-
-def get_artists(self, search_terms):
-        """Returns a Pandas DataFrame with selected artist info."""
+        artist_data = artist_response.json()
+        return artist_data
+    """
+        Defining columns and creating DataFrame
+        Returns DataFrame with artist details
+    """
+    def get_artists(self, search_terms):
         records = []
         for term in search_terms:
-            data = self.get_artist(term)['response']['artist']
-            records.append({
-                "search_term": term,
-                "artist_name": data.get("name"),
-                "artist_id": data.get("id"),
-                "follower_count": data.get("followers_count")
-            })
-        return pd.DataFrame(records)
+            artist_info = self.get_artist(term)
+            artist_data = artist_info['response']['artist']
+            record = {
+                'search_term': term,
+                'artist_name': artist_data.get('name'),
+                'artist_id': artist_data.get('id'),
+                'follower_count': artist_data.get('followers_count')
+            }
+            records.append(record)
+        df = pd.DataFrame(records)
+        return df
+genius = Genius()
+search_terms = ["Shaboozey", "Usher", "Taylor Swift", "Drake"]
+df = genius.get_artists(search_terms)
+print(df)
 
 
 
